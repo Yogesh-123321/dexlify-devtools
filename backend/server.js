4dbad94ad554e41f65d7cade9bea1bc6dce27a52
Yogesh Madan <yogeshmadan1428@gmail.com>
2025-07-18 16:35:17 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5500;
 
 app.use(cors({
-  origin: "https://dexlify-frontend.onrender.com/",
+  origin: "https://dexlify-frontend.onrender.com",
   credentials: true,
 }));
 
