9d98c2f268bcb2aa324f33fc3ae219285095498f
Yogesh Madan <yogeshmadan1428@gmail.com>
2025-11-07 12:26:21 +0530

    m,,

--- backend/routes/explainerRoute.js
+++ backend/routes/explainerRoute.js
@@ -16,7 +16,7 @@ router.post("/", detectUser, async (req, res) => {
     const response = await axios.post(
   "https://openrouter.ai/api/v1/chat/completions",
   {
-    model: "deepseek/deepseek-r1-0528:free",
+    model: "openai/gpt-4o-mini",
     messages: [
       {
         role: "system",
